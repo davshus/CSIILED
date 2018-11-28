bd1fda3a32cbe9e75d17fdafbe2376b1e1af51e0
David Shustin <david.shustin@gmail.com>
2018-11-28 09:36:13 -0500

    Initial Commit

---
+++ csiiled.py
@@ -0,0 +1,29 @@
+import board
+import neopixel
+
+led = neopixel.NeoPixel(board.D18, 270, auto_write = False)
+
+
+board = [[(0,0,0)] * 15] * 18
+
+def setPixel(x, y, color):
+    board[x][y] = color
+    led[positionAt(x,y)] = color
+    led.show()
+
+
+def setBoard(newBoard):
+    board = newBoard
+    reload()
+
+def reload():
+    for x in range(0,18):
+        for y in range(0,15):
+            led[positionAt(x,y)] = board[x][y]
+    led.show()
+
+def positionAt(x, y):
+    if x % 2 != 0:
+        return 252 - 18*x + y
+    else:
+        return 269 - 18*x - y
