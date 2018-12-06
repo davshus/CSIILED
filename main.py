111fdcea343a7c9d66b4509c9cd21c4e254293b4
David Shustin <david.shustin@gmail.com>
2018-12-05 19:26:08 -0500

    Add animation cycle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import time
 import md5
+import bitmap as bmp
+import glob
 
 default = getAnimationArray("main")
 cloud = getAnimationArray("cloud")
@@ -18,15 +20,16 @@ for animation in animations:
 #files named 1 to n, load each and put them into array
 #each bmp is a 2d array
 #end with a 3d array
-def getAnimationArray(folder): 
+def getAnimationArray(folder):
     os.chdir(folder)
     animation_array = []
 
-    for bmp in os.listdir():
-        animation_array.append(loadBitmap(bmp))
+    for bmp in [j + ".bmp" for j in sorted([i[:len(i)-4] for i in glob.glob("*.bmp")], key=int)]:
+        animation_array.append(bmp.load(bmp))
 
     return animation_array
 
+#TODO: implement multi-layered animation cycle using bmp.composite
 
 def displayAnimation(animation_array):
 
@@ -34,7 +37,7 @@ def displayAnimation(animation_array):
 		setBoard(bmp)
 		time.delay(1/30)
 
-def getGod(name): 
+def getGod(name):
 	m = md5.new()
 	m.update(name)
 	god_num = m.digest() % 12
@@ -42,5 +45,4 @@ def getGod(name):
 
 def main():
 	while True:
-		displayAnimation(default)
-		
+		displayAnimation(bmp.composite(default))
